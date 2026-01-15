118a8d36b26ff5a8aee851a43c70af343139786a
maxoiduss <maxoiduss@gmail.com>
2026-01-15 22:06:19 +0000

    Modern eslint.config.js instead of .eslintrc.json, linted the project

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  {
+    ignores: [
+      "out/",
+      "dist/",
+      "**/*.d.ts"
+    ],
+  },
+  {
+    files: [
+      "**/*.ts",
+      "**/*.tsx"
+    ],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module"
+      }
+    },
+    // register plugin module; require the plugin package so ESLint can resolve it
+    plugins: {
+      "@typescript-eslint": tsPlugin
+    },
+    rules: {
+      "@typescript-eslint/naming-convention": "warn",
+      "@typescript-eslint/semi": "warn",
+      "curly": "warn",
+      "eqeqeq": "warn",
+      "no-throw-literal": "warn",
+      "semi": "warn",
+    },
+  }
+];
