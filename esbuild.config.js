cbe970dda19eaaa1f0ae5989faf6dabbebf7f81d
maxoiduss <maxoiduss@gmail.com>
2026-01-16 04:44:04 +0000

    Esbuild file config, specifying using source maps

---
+++ esbuild.config.js
@@ -0,0 +1,34 @@
+/*function copyResources() {
+  const path = require('path');
+  const fs = require('fs');
+  const outRes = 'resources';
+  const src = path.resolve(outRes);
+  const dest = path.resolve(path.join(outDir, outRes));
+  fs.rmSync(dest, { recursive: true, force: true });
+  fs.cpSync(src, dest, { recursive: true });
+  console.log(outRes + ' copied');
+}*/
+
+const esbuild = require('esbuild');
+
+const outDir = 'dist';
+
+esbuild.build({
+  entryPoints: [
+    'src/extension.ts'                // your entry file
+  ],
+  bundle: true,                       // bundle dependencies
+  platform: 'node',                   // VS Code extensions run in Node
+  target: 'node18',                   // match VS Code’s runtime (Node 18+)
+  format: 'cjs',
+  outfile: `${outDir}/extension.js`,  // output file
+  external: [
+    'vscode',                         // keep VS Code API external
+  ],
+  mainFields: ['main'],
+  conditions: ['node'],               // ensure node-specific code is used
+  packages: 'bundle',                 // force bundling of all packages
+  sourcemap: true,                    // required for debugging
+  sourcesContent: true,               // binding to the source code
+  minify: false                       // optional, usually off for extensions
+}).catch(() => process.exit(1));
